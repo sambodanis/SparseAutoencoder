0e1aab9bfe8fca86fe0381dec848dcb16dac04d5
SamAditazz <sam.bodanis@gmail.com>
2013-06-19 11:46:40 -0700

    Initial Commit

---
+++ SparseAutoencoder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SparseAutoencoder
+//
+//  Created by Sam Bodanis on 19/06/2013.
+//  Copyright (c) 2013 Sam Bodanis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
